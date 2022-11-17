deab9a84ae74727cd9c27ae0fb60ad2c4330aedc
BONIFACE KORIR <boniface.korir@student.moringa.school@gmail.com>
2022-11-17 13:59:05 +0300

    arrays

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name)
+    return cats;
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+    return cats;
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats;
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats;
+}
+
+function appendCat(name){
+    let name_1= [...cats,name];
+    return name_1;
+}
+function prependCat(name){
+    let name_2=[name, ...cats];
+    return name_2
+  }
+
+  function removeLastCat(){
+    let pakas= cats.slice(0,cats.length-1);
+    return pakas;
+  }
+  
+  function removeFirstCat(){
+    let pakas= cats.slice(1)
+    return pakas
+  }
